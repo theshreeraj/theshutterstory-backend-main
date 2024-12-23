fecaf6679ae96d7e14192c35914a0e88554b5d81
shreeraj007 <shreerajmane007@gmail.com>
2024-12-23 11:41:24 +0530

    minor change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import reviewRoute from "./Routes/review.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
